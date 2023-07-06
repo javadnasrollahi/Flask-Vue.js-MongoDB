5ce0f19f476c76d4298ed9b3fc752bdb6187b615
Javad Nasrollahi <javadnasrollahi68@gmail.com>
2023-07-07 01:58:26 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Flask-Vue.js-MongoDB
---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, jsonify, abort,request
+from pydantic import BaseModel
+import pymongo
+import json
+
+db = "testdb"
+url = "localhost"
+port = 27017
+
+class SetEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+        return json.JSONEncoder.default(self, obj)
+class BLOG(BaseModel):
+    title: str
+    content: str
+
+mycol = None
+def connect():
+    global mycol
+    myclient = pymongo.MongoClient(url + ":" + str(port))
+    
+    if db not in myclient.list_database_names():
+        myclient[db]
+       
+    mydb = myclient[db]
+    mycol = mydb["Community"]
+    
+    return "Connected"
+
+connect()
+app = Flask(__name__, static_url_path='')
+@app.route("/", methods=["GET"])
+def root():
+    return app.send_static_file('index.html')
+
+@app.route("/items", methods=["GET"])
+def get_items():
+    result = []
+    for x in mycol.find():
+        print(type(x["_id"]))
+        result.append({
+            "_id":str((x["_id"])),
+            "title":x["title"],
+            "content":x["content"],
+            }
+        )
+    return jsonify({"success": True,"data":result})
+
+@app.route("/", methods=["POST"])
+def post_add():
+    x = mycol.insert_one(dict(request.get_json()))
+    print(x )
+    return jsonify({"data":"","success":True}), 201
+
+if __name__ == "__main__":
+    app.run(debug=True)
